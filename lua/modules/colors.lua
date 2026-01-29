f600e13f214bfb504dd7c1e99124f7d91bd3fecc
Rogerskelamen <rogerskelamen@gmail.com>
2026-01-29 23:53:01 +0800

    fix: fix bugs to support Windows system

--- lua/modules/colors.lua
+++ lua/modules/colors.lua
@@ -1,70 +1,87 @@
 ------------------------------------------------------------------------
 --                             Colours                                --
 ------------------------------------------------------------------------
-local config = require('modules.config')
+
+---@class ColorScheme
+---@field purple string
+---@field blue string
+---@field yellow string
+---@field green string
+---@field red string
+---@field black_fg string
+---@field inactive_bg string
+---@field white_fg string
+---@field statusline_bg string
+---@field statusline_fg string
+
+local config = require("modules.config")
 
 local M = {}
 
 -- Default colours
+---@type ColorScheme
 local defaults = {
-	-- Different colors for mode
-	purple = '#BF616A', --#B48EAD
-	blue = '#83a598', --#81A1C1
-	yellow = '#fabd2f', --#EBCB8B
-	green = '#8ec07c', --#A3BE8C
-	red = '#fb4934', --#BF616A
+  -- Different colors for mode
+  purple = "#BF616A", --#B48EAD
+  blue = "#83a598", --#81A1C1
+  yellow = "#fabd2f", --#EBCB8B
+  green = "#8ec07c", --#A3BE8C
+  red = "#fb4934", --#BF616A
 
-	-- fg and bg
-	white_fg = '#b8b894',
-	black_fg = '#282c34',
+  -- fg and bg
+  white_fg = "#b8b894",
+  black_fg = "#282c34",
 
-	-- Inactive bg
-	inactive_bg = '#1c1c1c',
+  -- Inactive bg
+  inactive_bg = "#1c1c1c",
 
-	-- Statusline colour
-	statusline_bg = 'NONE', -- Set to none, use native bg
-	statusline_fg = 'NONE',
+  -- Statusline colour
+  statusline_bg = "NONE", -- Set to none, use native bg
+  statusline_fg = "NONE",
 }
 
-local colors = nil
+---@type ColorScheme|nil
+local colors
 
 local function get_color(group, key, fallback)
-	local ok, hl = pcall(vim.api.nvim_get_hl_by_name, group, true)
-	if not ok or not config.get().match_colorscheme then
-		-- if not ok then
-		return fallback
-	end
-	local color = hl[key]
-	if not color then
-		return fallback
-	end
-	return string.format('#%06x', color)
+  local ok, hl = pcall(vim.nvim_get_hl, group, true)
+  if not ok or not config.get().match_colorscheme then
+    -- if not ok then
+    return fallback
+  end
+  local color = hl[key]
+  if not color then
+    return fallback
+  end
+  return string.format("#%06x", color)
 end
 
+---@return ColorScheme
 local function compute_colors()
-	colors = {
-		purple = get_color('Statement', 'foreground', defaults.purple),
-		blue = get_color('Function', 'foreground', defaults.blue),
-		yellow = get_color('Constant', 'foreground', defaults.yellow),
-		green = get_color('String', 'foreground', defaults.green),
-		red = get_color('Error', 'foreground', defaults.red),
-		white_fg = get_color('Normal', 'foreground', defaults.white_fg),
-		black_fg = get_color('Normal', 'background', defaults.black_fg),
-		inactive_bg = get_color('NormalNC', 'background', defaults.inactive_bg),
-		statusline_bg = defaults.statusline_bg,
-		statusline_fg = defaults.statusline_fg,
-	}
+  return {
+    purple = get_color("Statement", "foreground", defaults.purple),
+    blue = get_color("Function", "foreground", defaults.blue),
+    yellow = get_color("Constant", "foreground", defaults.yellow),
+    green = get_color("String", "foreground", defaults.green),
+    red = get_color("Error", "foreground", defaults.red),
+    white_fg = get_color("Normal", "foreground", defaults.white_fg),
+    black_fg = get_color("Normal", "background", defaults.black_fg),
+    inactive_bg = get_color("NormalNC", "background", defaults.inactive_bg),
+    statusline_bg = defaults.statusline_bg,
+    statusline_fg = defaults.statusline_fg,
+  }
 end
 
+---@return ColorScheme
 function M.get()
-	if not colors then
-		compute_colors()
-	end
-	return colors
+  if not colors then
+    colors = compute_colors()
+  end
+  return colors
 end
 
 function M.reset()
-	colors = nil
+  colors = nil
 end
 
 return M
--- lua/modules/tabline.lua
+++ lua/modules/tabline.lua
@@ -5,92 +5,97 @@ local M = {}
 
 local api = vim.api
 local cmd = api.nvim_command
-local icons = require('tables._icons')
-local config = require('modules.config')
+local icons = require("tables._icons")
+local config = require("modules.config")
 
 -- Separators
-local left_separator = ''
-local right_separator = ''
-local space = ' '
+local left_separator = ""
+local right_separator = ""
+local space = " "
 
+---Trim dir with '~' char and make sure it's less then 30
+---@param dir string
+---@return string
 local TrimmedDirectory = function(dir)
-	local home = os.getenv('HOME')
-	local _, index = string.find(dir, home, 1)
-	if index ~= nil and index ~= string.len(dir) then
-		if string.len(dir) > 30 then
-			dir = '..' .. string.sub(dir, 30)
-		end
-		return string.gsub(dir, home, '~')
-	end
-	return dir
+  local home = vim.uv.os_homedir()
+  if vim.startswith(dir, home) and #dir ~= #home then
+    if #dir > 30 then
+      dir = ".." .. dir:sub(30)
+    end
+    local new_dir = dir:gsub(home, "~")
+    return new_dir
+  end
+  return dir
 end
 
+---@param n integer
+---@return string
 local getTabLabel = function(n)
-	local current_win = api.nvim_tabpage_get_win(n)
-	local current_buf = api.nvim_win_get_buf(current_win)
-	local file_name = api.nvim_buf_get_name(current_buf)
-	if string.find(file_name, 'term://') ~= nil then
-		return ' ' .. api.nvim_call_function('fnamemodify', { file_name, ':p:t' })
-	end
-	file_name = api.nvim_call_function('fnamemodify', { file_name, ':p:t' })
-	if file_name == '' then
-		return 'No Name'
-	end
-	local icon = icons.deviconTable[file_name]
-	if icon ~= nil then
-		return icon .. space .. file_name
-	end
-	return file_name
+  local win = vim.api.nvim_tabpage_get_win(n)
+  local file_name = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
+  if vim.startswith(file_name, "term://") then
+    return " " .. vim.fs.basename(file_name)
+  end
+  file_name = vim.fs.basename(file_name)
+  if file_name == "" then
+    return "No Name"
+  end
+  local icon = icons.deviconTable[file_name]
+  if icon then
+    return icon .. space .. file_name
+  end
+  return file_name
 end
 
 local set_colours = function()
-	local colors = require('modules.colors').get()
-	cmd('hi TabLineSel gui=Bold guibg=' .. colors.green .. ' guifg=' .. colors.black_fg)
-	cmd('hi TabLineSelSeparator gui=bold guifg=' .. colors.green)
-	cmd('hi TabLine guibg=' .. colors.inactive_bg .. ' guifg=' .. colors.white_fg .. ' gui=None')
-	cmd('hi TabLineSeparator guifg=' .. colors.inactive_bg)
-	cmd('hi TabLineFill guibg=None gui=None')
+  ---@type ColorScheme
+  local colors = require("modules.colors").get()
+  cmd("hi TabLineSel gui=Bold guibg=" .. colors.green .. " guifg=" .. colors.black_fg)
+  cmd("hi TabLineSelSeparator gui=bold guifg=" .. colors.green)
+  cmd("hi TabLine guibg=" .. colors.inactive_bg .. " guifg=" .. colors.white_fg .. " gui=None")
+  cmd("hi TabLineSeparator guifg=" .. colors.inactive_bg)
+  cmd("hi TabLineFill guibg=None gui=None")
 end
 
 function M.init()
-	if not config.get().tabline then
-		return ''
-	end
+  if not config.get().tabline then
+    return ""
+  end
 
-	set_colours()
-	local tabline = ''
-	local tab_list = api.nvim_list_tabpages()
-	local current_tab = api.nvim_get_current_tabpage()
+  set_colours()
+  local tabline = ""
+  local tab_list = api.nvim_list_tabpages()
+  local current_tab = api.nvim_get_current_tabpage()
 
-	for _, val in ipairs(tab_list) do
-		local file_name = getTabLabel(val)
-		if val == current_tab then
-			tabline = tabline .. '%' .. val .. 'T'
-			tabline = tabline .. '%#TabLineSelSeparator# ' .. left_separator
-			tabline = tabline .. '%#TabLineSel# ' .. file_name
-			tabline = tabline .. ' %#TabLineSelSeparator#' .. right_separator
-			tabline = tabline .. '%T'
-		else
-			tabline = tabline .. '%' .. val .. 'T'
-			tabline = tabline .. '%#TabLineSeparator# ' .. left_separator
-			tabline = tabline .. '%#TabLine# ' .. file_name
-			tabline = tabline .. ' %#TabLineSeparator#' .. right_separator
-			tabline = tabline .. '%T'
-		end
-	end
+  for _, val in ipairs(tab_list) do
+    local file_name = getTabLabel(val)
+    if val == current_tab then
+      tabline = tabline .. "%" .. val .. "T"
+      tabline = tabline .. "%#TabLineSelSeparator# " .. left_separator
+      tabline = tabline .. "%#TabLineSel# " .. file_name
+      tabline = tabline .. " %#TabLineSelSeparator#" .. right_separator
+      tabline = tabline .. "%T"
+    else
+      tabline = tabline .. "%" .. val .. "T"
+      tabline = tabline .. "%#TabLineSeparator# " .. left_separator
+      tabline = tabline .. "%#TabLine# " .. file_name
+      tabline = tabline .. " %#TabLineSeparator#" .. right_separator
+      tabline = tabline .. "%T"
+    end
+  end
 
-	tabline = tabline .. '%='
-	local dir = api.nvim_call_function('getcwd', {})
-	tabline = tabline
-		.. '%#TabLineSeparator#'
-		.. left_separator
-		.. '%#Tabline# '
-		.. TrimmedDirectory(dir)
-		.. '%#TabLineSeparator#'
-		.. right_separator
+  tabline = tabline .. "%="
+  local dir = api.nvim_call_function("getcwd", {})
+  tabline = tabline
+  .. "%#TabLineSeparator#"
+  .. left_separator
+  .. "%#Tabline# "
+  .. TrimmedDirectory(dir)
+  .. "%#TabLineSeparator#"
+  .. right_separator
 
-	tabline = tabline .. space
-	return tabline
+  tabline = tabline .. space
+  return tabline
 end
 
 return M
