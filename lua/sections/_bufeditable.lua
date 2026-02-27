a1648c3c4dd187864a783e3b141e5b1c77edaa67
Rogerskelamen <rogerskelamen@gmail.com>
2026-02-27 19:26:37 +0800

    refactor(bufeditable): improve structure

--- lua/sections/_bufeditable.lua
+++ lua/sections/_bufeditable.lua
@@ -1,12 +1,23 @@
+local space = " "
+
 local M = {}
--- local vim = vim
-function M.editable()
-  if vim.bo.filetype == "help" then
+
+---Return read-only icon if file can't be modified
+---@param bufnr? integer
+---@return string
+function M.editable(bufnr)
+  bufnr = bufnr or 0
+  local bo = vim.bo[bufnr]
+
+  if bo.filetype == "help" then
     return ""
   end
-  if vim.bo.readonly == true then
-    return " "
+
+  if bo.readonly == true then
+    return "" .. space
   end
+
   return ""
 end
+
 return M
